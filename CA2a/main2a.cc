ad8856f3d10cbbf6bad0c820618bd3e7b14af059
Yiduo <wangyiduo999@gmail.com>
2017-04-11 00:28:42 +0000

    main format

--- CA2a/main2a.cc
+++ CA2a/main2a.cc
@@ -13,52 +13,56 @@ It uses the deal.II FEM library, dealii.org*/
 using namespace dealii;
 
 //The main program, using the FEM class
-int main (){
-  try{
-    deallog.depth_console (0);
-		
+int main () {
+	try {
+		deallog.depth_console (0);
+
 		const int dimension = 2;
 
-    FEM<dimension> problemObject;
+		FEM<dimension> problemObject;
 
 		//NOTE: This is where you define the number of elements in the mesh
 		std::vector<unsigned int> num_of_elems(dimension);
-		num_of_elems[0] = 4;
-		num_of_elems[1] = 8; //For example, a 4 x 8 element mesh in 2D
+		// num_of_elems[0] = 4;
+		// num_of_elems[1] = 8; //For example, a 4 x 8 element mesh in 2D
+
+
+		num_of_elems[0] = 5;
+		num_of_elems[1] = 13; //For example, a 5 x 13 element mesh in 2D
 
 		problemObject.generate_mesh(num_of_elems);
-	  problemObject.setup_system();
-	  problemObject.assemble_system();
-	  problemObject.solve();
+		problemObject.setup_system();
+		problemObject.assemble_system();
+		problemObject.solve();
 		problemObject.output_results();
 
-    //write solutions to h5 file
-    char tag[21];
-    sprintf(tag, "CA2a");
-    writeSolutionsToFileCA2(problemObject.D, tag);
-  }
-  catch (std::exception &exc){
-    std::cerr << std::endl << std::endl
-	      << "----------------------------------------------------"
-	      << std::endl;
-    std::cerr << "Exception on processing: " << std::endl
-	      << exc.what() << std::endl
-	      << "Aborting!" << std::endl
-	      << "----------------------------------------------------"
-	      << std::endl;
+		//write solutions to h5 file
+		char tag[21];
+		sprintf(tag, "CA2a");
+		writeSolutionsToFileCA2(problemObject.D, tag);
+	}
+	catch (std::exception &exc) {
+		std::cerr << std::endl << std::endl
+		          << "----------------------------------------------------"
+		          << std::endl;
+		std::cerr << "Exception on processing: " << std::endl
+		          << exc.what() << std::endl
+		          << "Aborting!" << std::endl
+		          << "----------------------------------------------------"
+		          << std::endl;
 
-    return 1;
-  }
-  catch (...){
-    std::cerr << std::endl << std::endl
-	      << "----------------------------------------------------"
-	      << std::endl;
-    std::cerr << "Unknown exception!" << std::endl
-	      << "Aborting!" << std::endl
-	      << "----------------------------------------------------"
-	      << std::endl;
-    return 1;
-  }
+		return 1;
+	}
+	catch (...) {
+		std::cerr << std::endl << std::endl
+		          << "----------------------------------------------------"
+		          << std::endl;
+		std::cerr << "Unknown exception!" << std::endl
+		          << "Aborting!" << std::endl
+		          << "----------------------------------------------------"
+		          << std::endl;
+		return 1;
+	}
 
-  return 0;
+	return 0;
 }
